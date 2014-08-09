abf8fffae7c23e589a6e0babdb6fe90bd39a4379
ArgonCode <argoncode18@gmail.com>
2014-08-09 16:21:35 +0300

    FizzBuzz with each

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,17 +1,40 @@
 def fizz_buzz_for_loop (start_n, stop_n)
-  for num in (start_n..stop_n) do 
+  reply = []
+    for num in (start_n..stop_n) do
     if num%5.0 == 0
       if num%3.0 == 0
-        puts "FizzBuzz"
+        answer = "FizzBuzz"
       else
-        puts "Buzz"
+        answer= "Buzz"
       end
     elsif num%3 == 0
-      puts "Fizz"
+      answer = "Fizz"
     else
-      puts num
+      answer = num
     end
+  reply << answer
   end
+  reply
 end
 
-fizz_buzz_for_loop(0,100)
+p fizz_buzz_for_loop(1,30)
+
+
+def fizz_buzz_each (start_n, stop_n)
+  reply = []
+  (start_n..stop_n).each do |num|
+    if num%3 == 0 and num%5 == 0
+      answer = "FizzBuzz"
+    elsif num%3 == 0
+      answer = "Fizz"
+    elsif num%5 == 0
+      answer= "Buzz"
+    else
+      answer = num
+    end
+  reply << answer
+  end
+  reply
+end
+
+p fizz_buzz_each(1,30)
