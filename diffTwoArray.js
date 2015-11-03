a5bb038557fd0034b9891e53db257d648c1d740c
argon <argoncode18@gmail.com>
2015-11-03 12:37:06 -0800

    Refactor Diff Between Two Arrays. JavaScript

--- diffTwoArray.js
+++ diffTwoArray.js
@@ -1,22 +1,19 @@
 /*
 Compare two arrays and return a new array with any items not found in both of the original arrays.
 */
-
 function diff(arr1, arr2) {
-  var arr1Copy = arr1;
-  
-  
   for(var i = arr1.length - 1 ; i >= 0; i--) {
     var element = arr1[i];
     
     while (arr2.indexOf(element) !== -1 ) {
       var idx = arr2.indexOf(element);
       arr2.splice(idx, 1);
-      arr1Copy.splice(i, 1);
+      arr1.splice(i, 1);
     }
   }
-  return arr1Copy.concat(arr2);
+  return arr1.concat(arr2);
 }
+
 
 diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 diff(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"])
