b5fdb18b9f553031d883bbd70c1762f674ed2670
argon <argoncode18@gmail.com>
2015-11-06 09:25:00 -0800

    Add solution to Truth test in Array. JavaScript

---
+++ truthInArray.js
@@ -0,0 +1,11 @@
+/*
+Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
+*/
+
+function find(arr, func) {
+  var num = arr.filter(func)[0];
+  return num;
+}
+
+find([1, 2, 3, 4], function(num){ return num % 2 === 0; });
+
